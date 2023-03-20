39be2b8aac35615274272b9280c24d6c4ee730c0
Reinaldo <reinaldoybk@hotmail.com>
2023-03-20 16:02:43 -0300

    home page

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,9 +1,55 @@
 import React from "react";
+import { Link } from 'react-router-dom';
+import './styles.scss';
 
 export const Home = () => {
+    const posts = [
+        {
+            id: 1,
+            title: 'Lorem ipsum dolor sit amet.',
+            desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, exercitationem?',
+            img: 'https://culturainglesacuritiba.com.br/wp-content/uploads/2020/09/outono-na-inglaterra.png'
+        },
+        {
+            id: 2,
+            title: 'Lorem ipsum dolor sit amet.',
+            desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, exercitationem?',
+            img: 'https://culturainglesacuritiba.com.br/wp-content/uploads/2020/09/outono-na-inglaterra.png'
+        },
+        {
+            id: 3,
+            title: 'Lorem ipsum dolor sit amet.',
+            desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, exercitationem?',
+            img: 'https://culturainglesacuritiba.com.br/wp-content/uploads/2020/09/outono-na-inglaterra.png'
+        },
+        {
+            id: 4,
+            title: 'Lorem ipsum dolor sit amet.',
+            desc: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt, exercitationem?',
+            img: 'https://culturainglesacuritiba.com.br/wp-content/uploads/2020/09/outono-na-inglaterra.png'
+        }
+    ]
+
     return (
         <div className="home">
-            from home
+            <div className="home-container">
+                <div className="posts">
+                    {posts.map(post =>(
+                        <div className="post" key={post.id}>
+                            <div className="img">
+                                <img src={post.img} alt='image' />
+                            </div>
+                            <div className="content">
+                                <Link className='link' to={`/post/${post.id}`}>
+                                <h1>{post.title}</h1>
+                                </Link>
+                                <p>{post.desc}</p>
+                                <button>Read more</button>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
         </div>
     )
 }
