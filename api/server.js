291d12b92594fdcb769a01054e85e082f62fa6d5
Reinaldo <reinaldoybk@hotmail.com>
2023-03-22 13:07:02 -0300

    backend routes

--- api/server.js
+++ api/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import * as dotenv from 'dotenv';
 import db from './db.js';
+import cors from 'cors';
 import authRoutes from "./routes/auth.js"
 import postRoutes from "./routes/posts.js"
 import userRoutes from "./routes/users.js"
@@ -8,12 +9,19 @@ import userRoutes from "./routes/users.js"
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 dotenv.config();
 
+// ROUTES
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/user", userRoutes);
 
-app.listen(process.env.PORT, () => {
-    console.log("Connected!")
+db.connect(err => {
+    if (err) throw err;
+    console.log("DB OK.")
+})
+
+app.listen(8000, () => {
+    console.log("Connected!");
 });
--- client/src/pages/register/Register.jsx
+++ client/src/pages/register/Register.jsx
@@ -1,17 +1,37 @@
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './styles.scss';
+import axios from 'axios';
 
 export const Register = () => {
+    const [inputs, setInputs] = useState({
+        username: "",
+        email: "",
+        password: ""
+    })
+
+    const handleChange = e => {
+        setInputs(prev => ({...prev, [e.target.name]: e.target.value}))
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        try {
+            const res = await axios.post("http://localhost:8000/api/auth/register", inputs)
+            console.log(res);
+        } catch (err) {
+            console.log(err);
+        }
+    }
+
     return (
         <div className='register'>
             <h2>Register</h2>
             <form>
-                <input required type='text' placeholder='First name' />
-                <input required type='text' placeholder='Last name' />
-                <input required type='email' placeholder='Email' />
-                <input required type='text' placeholder='Username' />
-                <input required type='password' placeholder='Password' />
-                <button>Register</button>
+                <input required type='text' placeholder='Username' name='username' onChange={handleChange} />
+                <input required type='email' placeholder='Email' name='email' onChange={handleChange} />
+                <input required type='password' placeholder='Password' name='password' onChange={handleChange} />
+                <button onClick={handleSubmit}>Register</button>
                 <p>Error area</p>
                 <span>Already have an account? <Link to='/login'>Login</Link></span>
             </form>
